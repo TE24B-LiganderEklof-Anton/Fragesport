a8f54687d19400bba75c4ce7ee0452e380ecd1c4
TE24B-Ligandereklof-Anton <anton.ligandereklof@elev.ga.ntig.se>
2025-09-09 14:03:48 +0200

    Added the questions and a loop to loop through them. Also copied and pasted the WriteAndAwaitValidAnswer method from the story project.

--- Fragesport/Program.cs
+++ Fragesport/Program.cs
@@ -2,6 +2,7 @@
 {
     while (true)
     {
+        Console.WriteLine("----------------------------------------------------------------------");
         Console.WriteLine(Text);
         for (int i = 0; i < ValidAnswers.Length; i++)
         {
@@ -13,27 +14,18 @@
         bool AnswerIsInt = int.TryParse(AnswerString, out AnswerInt);
         if (AnswerIsInt && AnswerInt < ValidAnswers.Length + 1 && AnswerInt > 0)
         {
-            Console.WriteLine(AnswerInt);
+            //Console.WriteLine(AnswerInt);
             return AnswerInt - 1;
         }
         Console.WriteLine("That is not an option, please try again.");
     }
 }
 
-
 List<(string text, string[] answers, int correct)> questions = [];
 
-questions.Add(("textfråga", ["svar1", "svar2"], 1));
-questions.Add(("textfråga", ["svar1", "svar2"], 1));
-questions.Add(("textfråga", ["svar1", "svar2"], 1));
-
-// for (int i = 0; i < questions[0].answers.Length; i++)
-// {
-//     System.Console.WriteLine(questions[0].answers[i]);
-// }
-
-
-
+questions.Add(("How tall is Mount Everest?", ["7998m","8849m", "8958m","9081m", "7889m"], 1));
+questions.Add(("How long is the great wall of China?", ["12896km", "15825km", "36578km", "28765km", "21196km"], 4));
+questions.Add(("Tokyo is the biggest city in the world. Roughly how many people live there?", ["17 million", "21 million","25 million", "28 million","37 milliom",], 4));
 
 while (true)
 {
@@ -55,8 +47,15 @@ while (true)
             Console.WriteLine("Incorrect!");
         }
     }
-    int correctrate = points / questionamount;
-    Console.WriteLine("You got correctrate% of question correct");
+    int correctrate = points * 100 / questionamount;
+    Console.WriteLine($"You got {correctrate}% of question correct");
+    string againQuestion = "Would you like to try again?";
+    string[] againChoices = ["Yes", "No"];
+    int againAnswer = WriteAndAwaitValidAnswer(againQuestion, againChoices);
+    if (againAnswer == 1)
+    {
+        break;
+    }
     
 }
 
